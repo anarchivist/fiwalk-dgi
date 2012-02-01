298db07bdfa40bf393ac36da825ca6cf4829d067
Mark A. Matienzo <mark@matienzo.org>
2012-02-01 17:43:02 -0500

    get-mediainfo.rb: add comments

--- get-mediainfo.rb
+++ get-mediainfo.rb
@@ -18,6 +18,8 @@ streams = Hash[ *info.streams.collect {|v| [info.streams.index(v), v]}.flatten ]
 # probably overkill, but gives us everything that mediainfo would return.
 streams.each do |stream_id, stream|
   stream_type = stream.instance_variable_get(:@stream_type)
+  # this should output something like "mediainfoStream0_type: general"
+  # but fiwalk is reading it in as "mediainfoStream__type: general"
   puts "mediainfoStream#{stream_id}_type: #{stream_type}"
   stream.parsed_response[stream_type].each do |element, data|
     puts "mediainfoStream#{stream_id}_#{element}: #{data}"
