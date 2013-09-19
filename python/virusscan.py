ce4b1e55a20d03b02d263ccf3af62ef809e0312a
Mark A. Matienzo <mark@matienzo.org>
2013-09-19 18:46:09 -0400

    Refactor virus scan script

--- python/virusscan.py
+++ python/virusscan.py
@@ -5,7 +5,7 @@ virusscan.py - Scan a bitstream for viruses; depends on clamd and pyclamd
 """
 
 import sys
-from optparse import OptionParser
+from argparse import ArgumentParser
 from pyclamd import pyclamd
 import dateutil.parser
 
@@ -27,22 +27,18 @@ def virusscan(fn, method='socket', socket='/tmp/clamd.socket'):
             out['virusSignature'] = vscan[fn][1]
     return out
 
-def main():
-    parser = OptionParser()
-    parser.add_option('-m', '--method', dest='method',
-        default='socket', help='define method of interaction with clamav')
-    parser.add_option('-s', '--socket', dest='socket',
-        default='/tmp/clamd.socket', help='clamav unix socket to use')
-    opts, args = parser.parse_args()
+def main(arglist=None):
+    if arglist == None:
+        arglist = sys.argv[1:]
+    parser = ArgumentParser()
+    parser.add_argument('file', default=[], metavar='FILE', help='File to identify')
+    parser.add_argument('-m', '--method', default='socket', action='store_true', help='define method of interaction with clamav')
+    parser.add_argument('-s', '--socket', default='/tmp/clamd.socket', action='store_true', help='clamav unix socket to use')
+    args = parser.parse_args(arglist)
 
-    if len(args) < 1:
-        parser.print_help()
-        exit(-1)
-        
-    filename = args[0]
-    scan = virusscan(filename, method=opts.method, socket=opts.socket)
+    scan = virusscan(args.file, method=args.method, socket=args.socket)
     for k, v in scan.items():
-        print k + ': ' + v
+        sys.stdout.write("%(k)s: %(v)s\n" % {'k': k, 'v': v})
 
 if __name__ == "__main__":
     sys.exit(main())
